e893ad38f1a5de35e9f91484e03949c8e7dc56f9
= <=>
2020-06-03 19:56:06 +0700

    add readme

---
+++ PracticeWithStatic/StaticVariables.py
@@ -0,0 +1,3 @@
+# Static variable and methods are used when we want to define some behaviour or property specific to the class and which is something common for all the class objects.
+# If you look closely, for a static method we don't provide the argument self because static methods don't operate on objects.
+1. Static
